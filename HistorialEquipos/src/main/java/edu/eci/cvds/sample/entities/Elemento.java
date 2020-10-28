39689a414c4f5b223abccbfd7190a5a6283917b4
Javier200151 <javier200151@gmail.com>
2020-10-28 10:03:15 -0500

    Creación de entidades

---
+++ HistorialEquipos/src/main/java/edu/eci/cvds/sample/entities/Elemento.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.cvds.sample.entities;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author javie
+ */
+public class Elemento implements Serializable{
+    public Elemento(int id, String nombre, String tipo, Equipo equipo)
+    {
+        
+    }
+}
---
+++ HistorialEquipos/src/main/java/edu/eci/cvds/sample/entities/Novedad.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.cvds.sample.entities;
+import java.util.Date;
+
+/**
+ *
+ * @author javie
+ */
+public class Novedad {
+    public Novedad (int id , Date fecha , String titulo, User responsable, String detalle, Equipo equipo, Elemento Elemento)
+    {
+        
+    }
+}
