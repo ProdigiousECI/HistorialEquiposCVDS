0703b198fb5624d675fee0745673cb578445b502
Andres Davila <oadavilaf18@gmail.com>
2020-10-29 16:51:25 -0500

    dependencias

--- HistorialEquipos/src/main/java/edu/eci/cvds/sample/entities/Novedad.java
+++ HistorialEquipos/src/main/java/edu/eci/cvds/sample/entities/Novedad.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package edu.eci.cvds.sample.entities;
 
 import java.io.Serializable;
@@ -11,7 +11,7 @@ public class Novedad implements Serializable{
 	private Date fecha;
 	private String titulo;
 	private String detalle;
-	
+	//fuquene es un manco hijueputa
 	
 	public Novedad() {
 		
@@ -57,28 +57,9 @@ public class Novedad implements Serializable{
 		this.detalle = detalle;
 	}
 
-	
-	
-	
-
-
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package edu.eci.cvds.sample.entities;
-import java.util.Date;
-
-/**
- *
- * @author javie
- */
-public class Novedad {
-    public Novedad (int id , Date fecha , String titulo, User responsable, String detalle, Equipo equipo, Elemento Elemento)
-    {
-        
-    }
->>>>>>> 39689a414c4f5b223abccbfd7190a5a6283917b4
 }
+	
+	
+
+
+
